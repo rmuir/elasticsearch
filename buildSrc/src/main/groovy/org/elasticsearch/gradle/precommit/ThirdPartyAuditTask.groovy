5a7143d1cc7773e3a18839507bfb5ca88040809b
Robert Muir <rmuir@apache.org>
2015-12-24 21:08:22 -0500

    current state

--- buildSrc/src/main/groovy/org/elasticsearch/gradle/precommit/ThirdPartyAuditTask.groovy
+++ buildSrc/src/main/groovy/org/elasticsearch/gradle/precommit/ThirdPartyAuditTask.groovy
@@ -16,25 +16,25 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package org.elasticsearch.gradle.precommit
+package org.elasticsearch.gradle.precommit;
 
-import org.apache.tools.ant.BuildEvent
-import org.apache.tools.ant.BuildException
-import org.apache.tools.ant.BuildListener
-import org.apache.tools.ant.BuildLogger
-import org.apache.tools.ant.DefaultLogger
-import org.apache.tools.ant.Project
-import org.elasticsearch.gradle.AntTask
-import org.gradle.api.artifacts.Configuration
-import org.gradle.api.file.FileCollection
+import org.apache.tools.ant.BuildEvent;
+import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.BuildListener;
+import org.apache.tools.ant.BuildLogger;
+import org.apache.tools.ant.DefaultLogger;
+import org.apache.tools.ant.Project;
+import org.elasticsearch.gradle.AntTask;
+import org.gradle.api.artifacts.Configuration;
+import org.gradle.api.file.FileCollection;
 
-import java.nio.file.FileVisitResult
-import java.nio.file.Files
-import java.nio.file.Path
-import java.nio.file.SimpleFileVisitor
-import java.nio.file.attribute.BasicFileAttributes
-import java.util.regex.Matcher
-import java.util.regex.Pattern
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Basic static checking to keep tabs on third party JARs
@@ -45,8 +45,10 @@ public class ThirdPartyAuditTask extends AntTask {
     private String[] excludes = new String[0];
     
     ThirdPartyAuditTask() {
-        dependsOn(project.configurations.testCompile)
-        description = "Checks third party JAR bytecode for missing classes, use of internal APIs, and other horrors'"
+        // we depend on this because its the only reliable configuration
+        // this probably makes the build slower: gradle you suck here when it comes to configurations, you pay the price.
+        dependsOn(project.configurations.testCompile);
+        description = "Checks third party JAR bytecode for missing classes, use of internal APIs, and other horrors'";
     }
     
     /**
@@ -56,7 +58,7 @@ public class ThirdPartyAuditTask extends AntTask {
     public void setExcludes(String[] classes) {
         for (String s : classes) {
             if (s.indexOf('*') != -1) {
-                throw new IllegalArgumentException("illegal third party audit exclusion: '" + s + "', wildcards are not permitted!")
+                throw new IllegalArgumentException("illegal third party audit exclusion: '" + s + "', wildcards are not permitted!");
             }
         }
         excludes = classes;
@@ -71,30 +73,30 @@ public class ThirdPartyAuditTask extends AntTask {
 
     // yes, we parse Uwe Schindler's errors to find missing classes. Just don't let him know!
     static final Pattern MISSING_CLASS_PATTERN =
-        Pattern.compile(/WARNING: The referenced class '(.*)' cannot be loaded\. Please fix the classpath\!/)
+        Pattern.compile(/WARNING: The referenced class '(.*)' cannot be loaded\. Please fix the classpath\!/);
         
     static final Pattern VIOLATION_PATTERN = 
-        Pattern.compile(/\s\sin ([a-zA-Z0-9\$\.]+) \(.*\)/)
+        Pattern.compile(/\s\sin ([a-zA-Z0-9\$\.]+) \(.*\)/);
 
     // we log everything (except missing classes warnings). Those we handle ourselves.
     static class EvilLogger extends DefaultLogger {
-        final Set<String> missingClasses = new TreeSet<>()
+        final Set<String> missingClasses = new TreeSet<>();
         final Map<String,List<String>> violations = new TreeMap<>();
-        String previousLine = null
+        String previousLine = null;
 
         @Override
         public void messageLogged(BuildEvent event) {
             if (event.getTask().getClass() == de.thetaphi.forbiddenapis.ant.AntTask.class) {
                 if (event.getPriority() == Project.MSG_WARN) {
-                    Matcher m = MISSING_CLASS_PATTERN.matcher(event.getMessage())
+                    Matcher m = MISSING_CLASS_PATTERN.matcher(event.getMessage());
                     if (m.matches()) {
-                        missingClasses.add(m.group(1).replace('.', '/') + ".class")
+                        missingClasses.add(m.group(1).replace('.', '/') + ".class");
                     }
                 } else if (event.getPriority() == Project.MSG_ERR) {
-                    Matcher m = VIOLATION_PATTERN.matcher(event.getMessage())
+                    Matcher m = VIOLATION_PATTERN.matcher(event.getMessage());
                     if (m.matches()) {
-                        String violation = previousLine + '\n' + event.getMessage()
-                        String clazz = m.group(1).replace('.', '/') + ".class"
+                        String violation = previousLine + '\n' + event.getMessage();
+                        String clazz = m.group(1).replace('.', '/') + ".class";
                         List<String> current = violations.get(clazz);
                         if (current == null) {
                             current = new ArrayList<>();
@@ -102,36 +104,43 @@ public class ThirdPartyAuditTask extends AntTask {
                         }
                         current.add(violation);
                     }
-                    previousLine = event.getMessage()
+                    previousLine = event.getMessage();
                 }
             }
-            super.messageLogged(event)
+            super.messageLogged(event);
         }
     }
 
     @Override
     protected BuildLogger makeLogger(PrintStream stream, int outputLevel) {
-        DefaultLogger log = new EvilLogger()
-        log.errorPrintStream = stream
-        log.outputPrintStream = stream
-        log.messageOutputLevel = outputLevel
-        return log
+        DefaultLogger log = new EvilLogger();
+        log.errorPrintStream = stream;
+        log.outputPrintStream = stream;
+        log.messageOutputLevel = outputLevel;
+        return log;
     }
 
     @Override
     protected void runAnt(AntBuilder ant) {
-        ant.project.addTaskDefinition('thirdPartyAudit', de.thetaphi.forbiddenapis.ant.AntTask)
+        Configuration configuration = project.configurations.findByName('runtime');
+        if (configuration == null) {
+            // some projects apparently do not have 'runtime'? what a nice inconsistency,
+            // basically only serves to waste time in build logic!
+            configuration = project.configurations.findByName('testCompile');
+        }
+        assert configuration != null;
+        ant.project.addTaskDefinition('thirdPartyAudit', de.thetaphi.forbiddenapis.ant.AntTask);
 
         // we only want third party dependencies.
-        FileCollection jars = project.configurations.testCompile.fileCollection({ dependency ->
+        FileCollection jars = configuration.fileCollection({ dependency ->
             dependency.group.startsWith("org.elasticsearch") == false
-        })
+        });
 
         // we don't want provided dependencies, which we have already scanned. e.g. don't
         // scan ES core's dependencies for every single plugin
-        Configuration provided = project.configurations.findByName('provided')
+        Configuration provided = project.configurations.findByName('provided');
         if (provided != null) {
-            jars -= provided
+            jars -= provided;
         }
 
         // no dependencies matched, we are done
@@ -141,29 +150,29 @@ public class ThirdPartyAuditTask extends AntTask {
 
         // print which jars we are going to scan, always
         // this is not the time to try to be succinct! Forbidden will print plenty on its own!
-        Set<String> names = new TreeSet<>()
+        Set<String> names = new TreeSet<>();
         for (File jar : jars) {
-            names.add(jar.getName())
+            names.add(jar.getName());
         }
-        logger.error("Scanning: " + names)
+        logger.error("Scanning: " + names);
 
         // TODO: forbidden-apis + zipfileset gives O(n^2) behavior unless we dump to a tmpdir first,
         // and then remove our temp dir afterwards. don't complain: try it yourself.
         // we don't use gradle temp dir handling, just google it, or try it yourself.
 
-        File tmpDir = new File(project.buildDir, 'tmp/thirdPartyAudit')
+        File tmpDir = new File(project.buildDir, 'tmp/thirdPartyAudit');
 
         // clean up any previous mess (if we failed), then unzip everything to one directory
-        ant.delete(dir: tmpDir.getAbsolutePath())
-        tmpDir.mkdirs()
+        ant.delete(dir: tmpDir.getAbsolutePath());
+        tmpDir.mkdirs();
         for (File jar : jars) {
-            ant.unzip(src: jar.getAbsolutePath(), dest: tmpDir.getAbsolutePath())
+            ant.unzip(src: jar.getAbsolutePath(), dest: tmpDir.getAbsolutePath());
         }
 
         // convert exclusion class names to binary file names
         String[] excludedFiles = new String[excludes.length];
         for (int i = 0; i < excludes.length; i++) {
-            excludedFiles[i] = excludes[i].replace('.', '/') + ".class"
+            excludedFiles[i] = excludes[i].replace('.', '/') + ".class";
         }
         Set<String> excludedSet = new TreeSet<>(Arrays.asList(excludedFiles));
 
@@ -174,7 +183,7 @@ public class ThirdPartyAuditTask extends AntTask {
             ant.thirdPartyAudit(internalRuntimeForbidden: true,
                             failOnUnsupportedJava: false,
                             failOnMissingClasses: false,
-                            classpath: project.configurations.testCompile.asPath) {
+                            classpath: configuration.asPath) {
                 fileset(dir: tmpDir)
             }
         } catch (BuildException ignore) {}
@@ -206,7 +215,7 @@ public class ThirdPartyAuditTask extends AntTask {
         // don't allow a broken classpath
         evilLogger.missingClasses.removeAll(excludedSet);
         if (!evilLogger.missingClasses.isEmpty()) {
-            throw new IllegalStateException("CLASSES ARE MISSING! " + prettySet(evilLogger.missingClasses));
+            throw new IllegalStateException("CLASSES ARE MISSING! " + evilLogger.missingClasses);
         }
 
         // don't use internal classes
@@ -216,17 +225,9 @@ public class ThirdPartyAuditTask extends AntTask {
         }
 
         // clean up our mess (if we succeed)
-        ant.delete(dir: tmpDir.getAbsolutePath())
+        ant.delete(dir: tmpDir.getAbsolutePath());
     }
 
-    private static Set<String> prettySet(Set<String> raw) {
-        Set<String> cooked = new TreeSet<>();
-        for (String element : raw) {
-            cooked.add("\n'" + element.substring(0, element.length() - 6).replace('/', '.') + "'")
-        }
-        return cooked;
-    }
-    
     /**
      * check for sheisty classes: if they also exist in the extensions classloader, its jar hell with the jdk!
      */
@@ -234,14 +235,14 @@ public class ThirdPartyAuditTask extends AntTask {
         // system.parent = extensions loader.
         // note: for jigsaw, this evilness will need modifications (e.g. use jrt filesystem!). 
         // but groovy/gradle needs to work at all first!
-        ClassLoader ext = ClassLoader.getSystemClassLoader().getParent()
-        assert ext != null
+        ClassLoader ext = ClassLoader.getSystemClassLoader().getParent();
+        assert ext != null;
         
         Set<String> sheistySet = new TreeSet<>();
         Files.walkFileTree(root, new SimpleFileVisitor<Path>() {
             @Override
             public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-                String entry = root.relativize(file).toString()
+                String entry = root.relativize(file).toString();
                 if (entry.endsWith(".class")) {
                     if (ext.getResource(entry) != null) {
                         sheistySet.add(entry);
